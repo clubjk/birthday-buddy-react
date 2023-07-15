ed3fe26bcc81a59ce09038f7f77ac7e4204985cd
clubjk <jk@jkcybersecurity.com>
2023-07-15 16:19:20 -0500

    major update

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,35 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
+import { useState } from 'react';
+import data from './data';
+import List from './List';
 function App() {
-  const [count, setCount] = useState(0)
-
+  const [people, setPeople] = useState(data);
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+    <main>
+      <section className='container'>
+        <h3>{people.length} birthdays today</h3>
+        <List people={people} />
+        <button
+          type='button'
+          className='btn btn-block'
+          onClick={() => setPeople([])}
+        >
+          clear all
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+      </section>
+    </main>
+  );
 }
 
-export default App
+export default App;
